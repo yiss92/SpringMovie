acaefe51386309106ee49c067b5bcae03dfcfe88
yiss92 <yiss92>
2015-10-21 17:21:02 +0900

    vo member

--- src/repository/MemberDao.java
+++ src/repository/MemberDao.java
@@ -1,5 +1,71 @@
 package repository;
 
 public class MemberDao {
+	private int memberNum;
+	private String id;
+	private String password;
+	private String name;
+	private String nickname;
+	private String email;		
+	
+	public MemberDao() {
+		super();
+	}
 
+	public MemberDao(String id, String password, String name, String nickname, String email) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.nickname = nickname;
+		this.email = email;
+	}
+
+	public int getMemberNum() {
+		return memberNum;
+	}
+	
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
