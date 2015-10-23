ecb3ae390f7fa6f506388201c63bb4dad22eb66b
happyljh <hambaksmile@naver.com>
2015-10-23 17:52:56 +0900

    게시판 항목 수정

--- src/vo/FreeBoard.java
+++ src/vo/FreeBoard.java
@@ -3,68 +3,65 @@ package vo;
 import java.util.Date;
 
 public class FreeBoard {
-	
-	private String messageId;
-	private String guestName;
-	private String password;
+
+	private int boardNo;
+	private String id;
 	private String title;
 	private String comment;
 	private Date date;
-	private int boardPage;
-//////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////////////////////////
 	public FreeBoard() {
 	}
-	public FreeBoard(String messageId, String guestName, String password, String title, String comment, Date date, int boardPage) {
-		this.messageId = messageId;
-		this.guestName = guestName;
-		this.password = password;
+
+	public FreeBoard(String id, String title, String comment, Date date) {
+		super();
+		this.id = id;
 		this.title = title;
 		this.comment = comment;
 		this.date = date;
-		this.boardPage = boardPage;
 	}
-//////////////////////////////////////////////////////////////////////////	
-	public String getMessageId() {
-		return messageId;
+	//////////////////////////////////////////////////////////////////////////
+	
+	public int getBoardNo() {
+		return boardNo;
 	}
-	public void setMessageId(String messageId) {
-		this.messageId = messageId;
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
 	}
-	public String getGuestName() {
-		return guestName;
+
+	public String getId() {
+		return id;
 	}
-	public void setGuestName(String guestName) {
-		this.guestName = guestName;
+
+	public void setId(String id) {
+		this.id = id;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	public int getBoardPage() {
-		return boardPage;
-	}
-	public void setBoardPage(int boardPage) {
-		this.boardPage = boardPage;
-	}
 	
+
 }
